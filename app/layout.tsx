e15ada8a75a2409cf23628901e2063d33dd5cbdb
Ignacio Gomez <Nachorojo2015@gmail.com>
2026-01-22 20:14:57 -0300

    update SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,38 @@ import { Toaster } from "sonner";
 import { Providers } from "@/components/providers/Providers";
 
 export const metadata: Metadata = {
-  title: "Chat Next App",
-  description: "Una aplicación de chat web construida con Next.js",
+  metadataBase: new URL("https://chat-next-two.vercel.app"),
+
+  title: "Chat Next",
+  description: "Unete a chat next para conocer miles de grupos creados por la comunidad. Busca contenido de tu interes y disfruta.",
+
+  alternates: {
+    canonical: "https://chat-next-two.vercel.app",
+  },
+
+  openGraph: {
+    title: "Chat Next",
+    description: "Unete a chat next para conocer miles de grupos creados por la comunidad. Busca contenido de tu interes y disfruta.",
+    url: "https://chat-next-two.vercel.app",
+    siteName: "Mi Sitio",
+    images: [
+      {
+        url: "/logo-app.png",
+        width: 1200,
+        height: 630,
+        alt: "logo aplicattion",
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Chat Next",
+    description: "Unete a chat next para conocer miles de grupos creados por la comunidad. Busca contenido de tu interes y disfruta.",
+    images: ["/logo-app.png"],
+  },
 };
 
 export default function RootLayout({
@@ -15,9 +45,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" data-theme="light">
-      <body
-        className={`antialiased`}
-      >
+      <body className={`antialiased`}>
         <Toaster />
         <Providers>{children}</Providers>
       </body>
